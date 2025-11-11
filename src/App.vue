23ed323a0f544a56002caa9ad902cd92a70d5d04
Eric Niemeyer <niemeyer.eric@gmail.com>
2025-11-11 05:26:45 -0500

    name

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
-import FunctionApproximation from './components/FunctionApproximation.vue'
+import FunctionApproximation from './components/FunctionApproximation.vue';
 </script>
 
 <template>
-  <div class="w-screen h-screen flex flex-col p-2 text-center box-border overflow-hidden">
-    <h1 class="m-0 mb-2 text-primary text-2xl">Function Approximation</h1>
+  <div
+    class="w-screen h-screen flex flex-col p-2 text-center box-border overflow-hidden"
+  >
+    <h1 class="m-0 mb-2 text-2xl">
+      Genetic Algorithm Polynomal Function Approximation
+    </h1>
     <FunctionApproximation />
   </div>
 </template>
